ec976af83a3a0189ac4fc655ba1ec2e6675d7268
yfk123-code <75697534+yfk123-code@users.noreply.github.com>
2025-02-21 20:38:05 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  API_KEY: import.meta.env.VITE_API_KEY || 'AIzaSyDuaFrtqW_rU_xTqOE22JyLrBan4OttiAM', // Fallback API Key
-  SHEET_ID: import.meta.env.VITE_SHEET_ID || '14p7uHj8Utrg3J4FuOsmQtWWohqnd3uG0-BLViMRR3RA', // Fallback Sheet ID
-  SHEET_NAME: 'Sheet1' // Sheet name (isame change nahi karna hai)
-};
-
-export default config;
