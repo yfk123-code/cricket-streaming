2e2fc31cfdbfeb122012cf74a8a924db473bcea7
yfk123-code <75697534+yfk123-code@users.noreply.github.com>
2025-02-21 20:38:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js (Configuration File)
+const config = {
+  API_KEY: 'AIzaSyDuaFrtqW_rU_xTqOE22JyLrBan4OttiAM', // ✅ API Key Set
+  SHEET_ID: '14p7uHj8Utrg3J4FuOsmQtWWohqnd3uG0-BLViMRR3RA', // ✅ Sheet ID Set
+  SHEET_NAME: 'Sheet1'
+};
+export default config;
