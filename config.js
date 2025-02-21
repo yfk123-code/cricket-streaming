7730a886b14d5dc9a6e93e1794f8f371a2a659ff
yfk123-code <75697534+yfk123-code@users.noreply.github.com>
2025-02-21 21:47:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const config = {
+  API_KEY: 'AIzaSyDuaFrtqW_rU_xTqOE22JyLrBan4OttiAM', // Google Sheets API Key
+  SHEET_ID: '14p7uHj8Utrg3J4FuOsmQtWWohqnd3uG0-BLViMRR3RA', // Google Sheet ID
+  SHEET_NAME: 'Sheet1' // Sheet Name (change if different)
+};
+
+export default config;
